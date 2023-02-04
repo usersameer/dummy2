a68c0314c3e560a350a90914d9f7c8848e1a9d6d
usersameer <sameersalmani0503@gmail.com>
2023-02-04 06:59:02 +0530

    second commit

---
+++ code-clone/src/App.js
@@ -0,0 +1,16 @@
+//import logo from './logo.svg';
+//import './App.css';
+
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <div className="App"> 
+      {/* <h1>hey there...!</h1> */}
+      <Header></Header>
+    </div>
+
+  );
+}
+
+export default App;
